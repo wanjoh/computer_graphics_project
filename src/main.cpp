4e6c3738771803943a271134e8466ec51bb815e9
spaske00 <matf.marko.spasic@gmail.com>
2020-10-22 14:16:47 +0200

    main skelet

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,52 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+const int windowWidth = 800;
+const int windowHeight = 600;
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
 int main() {
 
+    if (!glfwInit()) {
+        std::cerr << "Failed to init glfw\n";
+        return EXIT_FAILURE;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "project_base", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cerr << "Failed to create a window\n";
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, keyCallback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to load gl functions\n";
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClearColor(0.3f, 0.2f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
