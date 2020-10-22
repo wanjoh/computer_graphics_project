3d0fa41163033f388dbb8f86d327279535f86e2b
spaske00 <matf.marko.spasic@gmail.com>
2020-10-22 16:45:12 +0200

    added error handling

---
+++ include/Error.h
@@ -0,0 +1,54 @@
+//
+// Created by spaske on 22.10.20..
+//
+
+#ifndef PROJECT_BASE_ERROR_H
+#define PROJECT_BASE_ERROR_H
+
+#include <iostream>
+#include <glad/glad.h>
+
+namespace rg {
+    
+    
+#define BREAK_IF_FALSE(x) if (!(x)) __builtin_trap()
+#define ASSERT(x, msg) do { std::cerr << msg << '\n'; BREAK_IF_FALSE(false); } while(0)
+#define GLCALL(x) \
+do { clearAllErrors; x; BREAK_IF_FALSE(wasPreviousOpenGLCallSuccessful(__FILE__, __LINE__, #x)); } while (0)
+
+    
+void clearAllOpenGlErrors();
+const char* openGLErrorToString(GLenum error);
+bool wasPreviousOpenGLCallSuccessful(const char* file, int line, const char* call);
+
+    void clearAllOpenGlErrors() {
+        while (glGetError() != GL_NO_ERROR) {
+            ;
+        }
+    }
+    const char* openGLErrorToString(GLenum error) {
+        switch(error) {
+            case GL_NO_ERROR: return "GL_NO_ERROR";
+            case GL_INVALID_ENUM: return "GL_INVALID_ENUM";
+            case GL_INVALID_VALUE: return "GL_INVALID_VALUE";
+            case GL_INVALID_OPERATION: return "GL_INVALID_OPERATION";
+            case GL_OUT_OF_MEMORY: return "GL_OUT_OF_MEMORY";
+        }
+        ASSERT(false, "Passed something that is not an error code");
+        return "THIS_SHOULD_NEVER_HAPPEN";
+    }
+    bool wasPreviousOpenGLCallSuccessful(const char* file, int line, const char* call) {
+        bool success = false;
+        while (GLenum error = glGetError()) {
+            std::cerr << "[OpenGL error] " << error << " " << openGLErrorToString(error)
+            << "\nFile: " << file
+            << "\nLine: " << line
+            << "\nCall: " << call
+            << "\n\n";
+            success = false;
+        }
+        return success;
+    }
+
+};
+#endif //PROJECT_BASE_ERROR_H
