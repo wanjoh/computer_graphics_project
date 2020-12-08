f0ab7e2663a8e9d24ac36b9728b5ee8d32e5943e
matf-rg <spaskeasm@gmail.com>
2020-12-08 19:54:13 +0100

    delete rule for ignoring

---
+++ resources/shaders/2.model_lighting.fs
@@ -0,0 +1,58 @@
+#version 330 core
+out vec4 FragColor;
+
+struct PointLight {
+    vec3 position;
+
+    vec3 specular;
+    vec3 diffuse;
+    vec3 ambient;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+struct Material {
+    sampler2D texture_diffuse1;
+    sampler2D texture_specular1;
+
+    float shininess;
+};
+in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
+
+uniform PointLight pointLight;
+uniform Material material;
+
+uniform vec3 viewPosition;
+// calculates the color when using a point light.
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    vec3 lightDir = normalize(light.position - fragPos);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // attenuation
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    // combine results
+    vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords).xxx);
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+    return (ambient + diffuse + specular);
+}
+
+void main()
+{
+    vec3 normal = normalize(Normal);
+    vec3 viewDir = normalize(viewPosition - FragPos);
+    vec3 result = CalcPointLight(pointLight, normal, FragPos, viewDir);
+    FragColor = vec4(result, 1.0);
+}
